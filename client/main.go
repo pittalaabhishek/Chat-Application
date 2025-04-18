0d102f421c752a38ba0a6704964f2b9cf8412296
pittalaabhishek <pittalaabhishek999@gmail.com>
2025-04-18 17:39:46 +0530

    Refactor go routines to use independent functions

--- client/main.go
+++ client/main.go
@@ -15,6 +15,120 @@ import (
 	pb "chat-application/gen/chat"
 )
 
+func handleCommand(text, username string, currentRoom string, client pb.ChatServiceClient, currentRoomPtr *string) {
+	switch {
+	case strings.HasPrefix(text, "/private "):
+		handlePrivateMessage(text, username, client)
+	case strings.HasPrefix(text, "/join "):
+		handleJoinRoom(text, username, client, currentRoomPtr)
+	case strings.HasPrefix(text, "/exit"):
+		handleExitRoom(username, client, currentRoomPtr)
+	default:
+		fmt.Println("Unknown command")
+	}
+}
+
+func handlePrivateMessage(text, username string, client pb.ChatServiceClient) {
+	parts := strings.SplitN(text, " ", 3)
+	if len(parts) < 3 {
+		fmt.Println("Usage: /private <user> <message>")
+		return
+	}
+	to := parts[1]
+	content := parts[2]
+
+	resp, err := client.SendPrivateMessage(context.Background(), &pb.PrivateMessageRequest{
+		From:    username,
+		To:      to,
+		Content: content,
+	})
+	if err != nil {
+		log.Printf("error sending private message: %v", err)
+		return
+	}
+	if !resp.Success {
+		log.Printf("failed to send private message: %s", resp.Message)
+	}
+}
+
+func handleJoinRoom(text, username string, client pb.ChatServiceClient, currentRoomPtr *string) {
+	if *currentRoomPtr != "" {
+		fmt.Printf("You are already in room '%s'. Please exit first with /exit\n", *currentRoomPtr)
+		return
+	}
+
+	room := strings.TrimPrefix(text, "/join ")
+	room = strings.TrimSpace(room)
+	*currentRoomPtr = room
+
+	roomStream, err := client.JoinChatRoom(context.Background(), &pb.ChatRoomRequest{
+		User: username,
+		Room: room,
+	})
+	if err != nil {
+		log.Printf("error joining room: %v", err)
+		return
+	}
+
+	go func() {
+		for {
+			msg, err := roomStream.Recv()
+			if err != nil {
+				log.Printf("room stream error: %v", err)
+				return
+			}
+			fmt.Printf("[%s] %s: %s\n", room, msg.User, msg.Content)
+		}
+	}()
+}
+
+func handleExitRoom(username string, client pb.ChatServiceClient, currentRoomPtr *string) {
+	if *currentRoomPtr == "" {
+		fmt.Println("Not currently in any room")
+		return
+	}
+
+	exitReq := &pb.ChatRoomRequest{
+		User: username,
+		Room: "!exit",
+	}
+
+	exitStream, err := client.JoinChatRoom(context.Background(), exitReq)
+	if err != nil {
+		log.Printf("error leaving room: %v", err)
+		return
+	}
+
+	msg, err := exitStream.Recv()
+	if err != nil {
+		log.Printf("error receiving exit confirmation: %v", err)
+		return
+	}
+
+	fmt.Println(msg.Content)
+	*currentRoomPtr = ""
+}
+
+func sendChatMessage(text, username, currentRoom string, stream pb.ChatService_ChatStreamClient) {
+	var msg *pb.ChatMessage
+	if currentRoom != "" {
+		msg = &pb.ChatMessage{
+			User:    username,
+			Content: text,
+			Room:    currentRoom,
+		}
+	} else {
+		msg = &pb.ChatMessage{
+			User:    username,
+			Content: text,
+		}
+	}
+
+	if err := stream.Send(msg); err != nil {
+		log.Printf("error sending message: %v", err)
+	}
+}
+
 func main() {
 	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -54,113 +168,12 @@ func main() {
 			text, _ := reader.ReadString('\n')
 			text = strings.TrimSpace(text)
 
-			if strings.HasPrefix(text, "/private ") {
-				parts := strings.SplitN(text, " ", 3)
-				if len(parts) < 3 {
-					fmt.Println("Usage: /private <user> <message>")
-					continue
-				}
-				to := parts[1]
-				content := parts[2]
-
-				resp, err := client.SendPrivateMessage(context.Background(), &pb.PrivateMessageRequest{
-					From:    username,
-					To:      to,
-					Content: content,
-				})
-				if err != nil {
-					log.Printf("error sending private message: %v", err)
-					continue
-				}
-				if !resp.Success {
-					log.Printf("failed to send private message: %s", resp.Message)
-				}
+			if strings.HasPrefix(text, "/") {
+				handleCommand(text, username, currentRoom, client, &currentRoom)
 				continue
 			}
 
-			if strings.HasPrefix(text, "/join ") {
-				if currentRoom != "" {
-					fmt.Printf("You are already in room '%s'. Please exit first with /exit\n", currentRoom)
-					continue
-				}
-				room := strings.TrimPrefix(text, "/join ")
-				room = strings.TrimSpace(room)
-				currentRoom = room
-
-				roomStream, err := client.JoinChatRoom(context.Background(), &pb.ChatRoomRequest{
-					User: username,
-					Room: room,
-				})
-				if err != nil {
-					log.Printf("error joining room: %v", err)
-					continue
-				}
-
-				go func() {
-					for {
-						msg, err := roomStream.Recv()
-						if err != nil {
-							log.Printf("room stream error: %v", err)
-							return
-						}
-						fmt.Printf("[%s] %s: %s\n", room, msg.User, msg.Content)
-					}
-				}()
-				continue
-			}
-
-			if strings.HasPrefix(text, "/exit") {
-				if currentRoom == "" {
-					fmt.Println("Not currently in any room")
-					continue
-				}
-			
-				// Send special "exit" request
-				exitReq := &pb.ChatRoomRequest{
-					User: username,
-					Room: "!exit", // Special room name to indicate exit
-				}
-			
-				// We reuse the JoinChatRoom RPC but with special room name
-				exitStream, err := client.JoinChatRoom(context.Background(), exitReq)
-				if err != nil {
-					log.Printf("error leaving room: %v", err)
-					continue
-				}
-			
-				// Wait for the exit confirmation
-				msg, err := exitStream.Recv()
-				if err != nil {
-					log.Printf("error receiving exit confirmation: %v", err)
-					continue
-				}
-			
-				fmt.Println(msg.Content)
-				currentRoom = ""
-				continue
-			}
-
-			// Send regular chat message
-			if currentRoom != "" {
-				// Send room-specific message
-				if err := stream.Send(&pb.ChatMessage{
-					User:    username,
-					Content: text,
-					Room:    currentRoom,  // Add this line
-				}); err != nil {
-					log.Printf("error sending message: %v", err)
-					return
-				}
-			} else {
-				// Send general chat message
-				if err := stream.Send(&pb.ChatMessage{
-					User:    username,
-					Content: text,
-				}); err != nil {
-					log.Printf("error sending message: %v", err)
-					return
-				}
-			}
+			sendChatMessage(text, username, currentRoom, stream)
 		}
 	}()
 
@@ -174,7 +187,15 @@ func main() {
 				return
 			}
 			if msg.User != username { // Don't echo our own messages
-				fmt.Printf("\n%s: %s\n> ", msg.User, msg.Content)
+				if msg.Room != "" && msg.Room != currentRoom {
+					// Skip messages from other rooms
+					continue
+				}
+				roomPrefix := ""
+				if msg.Room != "" {
+					roomPrefix = fmt.Sprintf("[%s] ", msg.Room)
+				}
+				fmt.Printf("\n%s%s: %s\n> ", roomPrefix, msg.User, msg.Content)
 			}
 		}
 	}()
